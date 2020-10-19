117210065569c1e3b165181448f78643aa0c272e
Kozuchowski <marekkozuchowski@vp.pl>
2020-10-19 11:55:54 +0200

    Updates

---
+++ precache-manifest.142744bbfbfd92d576da3e3a96278314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3502dff5a7bd59e29e1cdd20b64e15",
+    "url": "/full-react-dev-udemy/index.html"
+  },
+  {
+    "revision": "19071cfd70a6e8939d73",
+    "url": "/full-react-dev-udemy/static/css/main.52dd326c.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/full-react-dev-udemy/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/full-react-dev-udemy/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19071cfd70a6e8939d73",
+    "url": "/full-react-dev-udemy/static/js/main.ec9e89aa.chunk.js"
+  },
+  {
+    "revision": "343517183ce85b116bdf",
+    "url": "/full-react-dev-udemy/static/js/runtime-main.c066d0f3.js"
+  }
+]);
